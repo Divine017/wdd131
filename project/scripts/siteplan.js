db085a84145a7c8450c449d2cf9ce2c93cb37dc6
Divine017 <oye24005@byui.edu>
2025-02-12 10:03:16 +0100

    done

---
+++ project/scripts/siteplan.js
@@ -0,0 +1,8 @@
+// getdates.js
+document.addEventListener("DOMContentLoaded", function() {
+    const currentYear = new Date().getFullYear();
+    const lastModified = document.lastModified;
+
+    document.getElementById("currentyear").textContent = currentYear;
+    document.getElementById("lastModified").textContent = "Last Modified: " + lastModified;
+});
