f6a0268be610082a5b75fa3813a2d9efa6564e06
Divine017 <oye24005@byui.edu>
2025-02-13 04:44:08 +0100

    done

--- scripts/form-script.js
+++ scripts/form-script.js
@@ -1,10 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
     const products = [
-        { id: "fc-1888", name: "Flux Capacitor", averagerating: 4.5 },
-        { id: "fc-2050", name: "Power Laces", averagerating: 4.7 },
-        { id: "fs-1987", name: "Time Circuits", averagerating: 3.5 },
-        { id: "ac-2000", name: "Low Voltage Reactor", averagerating: 3.9 },
-        { id: "jj-1969", name: "Warp Equalizer", averagerating: 5.0 }
+        { id: "fc-1888", name: "Latops", averagerating: 4.5 },
+        { id: "fc-2050", name: "Smartphones", averagerating: 4.7 },
+        { id: "fs-1987", name: "Tablets", averagerating: 3.5 },
+        { id: "ac-2000", name: "Headphones", averagerating: 3.9 },
+        { id: "jj-1969", name: "Iphones", averagerating: 5.0 },
+        { id: "jj-1999", name: "Smartwatches", averagerating: 4.0 },
     ];
 
     const productSelect = document.getElementById("product");
@@ -33,3 +34,11 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("currentyear").textContent = currentYear;
     document.getElementById("lastModified").textContent = "Last Modified: " + lastModified;
 });
+
+// Retrieve and increment review count from localStorage
+let count = localStorage.getItem("reviewCount");
+count = count ? parseInt(count) + 1 : 1;
+localStorage.setItem("reviewCount", count);
+
+// Display updated count
+document.getElementById("review-count").textContent = count;
